b669515f41e74ec051f27cf3157093b8126f9c6b
Matt Lovan <magrelo404@gmail.com>
2018-06-20 11:32:04 -0400

    update contract

--- contracts/Servesa.sol
+++ contracts/Servesa.sol
@@ -1,31 +1,39 @@
 pragma solidity 0.4.24;
 
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-import "./BancorFormula.sol";
-
+// import "./BancorFormula.sol";
+// import "./Power.sol";
 // TEST: "0x106F681949E222D57A175cD85685E3bD9975b973","TEST", "TEST", 100000000000000, 300000, 1000
 
-contract Servesa is ERC721Token, BancorFormula {
+contract Servesa is ERC721Token {
+  using SafeMath for uint256;
 
   uint public version = 0;
+  uint32 MAX_WEIGHT = 1000000;
+  
   address public owner;
   address public provider;
   string public tokenName;
   string public tokenSymbol;
   
-  uint256 public tokenBasePrice = 100000000000000;
-  uint256 public tokenReserveRatio = 300000;
+  uint256 public tokenBasePrice = 100000000000;
+  uint32 public tokenReserveRatio = 300000;
   uint256 public maxTokens = 1000;
   
-  uint256 public totalCurrentTokens = 0;
-  uint256 public contractBalance = 0;
+
+  uint256 public totalTokensSupply = 0;
+  uint256 public contractBalance = 1;
+  
+  // Store the value of each token
+  mapping(uint256 => uint256) internal tokenValues;
 
   event NewContract(address ownerAddress, string tokenSymbol);
-  event Buy(address indexed funder, uint tokenCount);
-  event Sell(address indexed funder, uint value);
-  event Transfer(address indexed _from, address indexed _to, uint value);
   event FallbackEvent(address sender, uint amount);
+  event Buy(address sender, uint256 valueSent, uint256 tokenID, uint256 tokenUnitValue);
+  event Transfer(address sender, uint256 tokenUnitValue);
+
 
 
   /*
@@ -37,7 +45,7 @@ contract Servesa is ERC721Token, BancorFormula {
     string tokenNameInit,
     string tokenSymbolInit,
     uint256 tokenBasePriceInit,
-    uint256 tokenReserveRatioInit,
+    uint32 tokenReserveRatioInit,
     uint256 maxTokensInit) ERC721Token (tokenNameInit, tokenSymbolInit) public {
 
     // setup parent contract as provider
@@ -57,7 +65,7 @@ contract Servesa is ERC721Token, BancorFormula {
 
   // modifiers
   modifier belowMaxTokens() {
-    require(totalCurrentTokens < maxTokens);
+    require(totalTokensSupply < maxTokens);
     _;
   }
 
@@ -71,94 +79,65 @@ contract Servesa is ERC721Token, BancorFormula {
   /*
   * Buy: exchange ETH for tokens
   */
-  function buy() public payable {
-
-    /*
-    * Calc price
-    */
-    // uint tokenPrice = tokenBasePrice;
-    
-    /*
-    * Calc tokens
-    */
-    uint256 tokensToMint = calculatePurchaseReturn(totalCurrentTokens, contractBalance, 300000, msg.value);
-    
-    /*
-    * add token count to metadata - we'll actually only create one ERC-721 token
-    */
-
-
-    /*
-    * Create new tokens for sender
-    */
-    // super._mint(msg.sender, tokenId);
-
-    /*
-    * Refund overage
-    */
-
-    // event
-    emit Buy(msg.sender, tokensToMint);
-  }
-
-  /*
-  * Sell: exchange tokens for ETH
-  */
-  function sell(uint amount) public {
-
-    /*
-    * Calc ETH value
-    */
-
-
-    /*
-    * burn tokens
-    */
-
-
-    /*
-    * Send ETH
-    */
-
-    // event
-    emit Sell(msg.sender, 0);
-  }
-
-  /*
-  * Transfer: change owner of tokens
-  */
-  function transfer(uint amount) public {
+  function buy() payable public  {
 
     
+    // price     
+    require(msg.value > tokenBasePrice);
+    uint256 tokenUnitValue = _calcPurchaseReturn(msg.value);
+    
     /*
-    * Implement ERC721 safeTransfer
+    * create ERC721
     */
+    uint256 tokenNumber = allTokens.length;
+    super._mint(msg.sender, tokenNumber);
+    
+    /*
+    * add tokenUnitValue to metadata
+    */
+    _setTokenValue(tokenNumber, tokenUnitValue);
+
 
     // event
-    // emit Transfer('0x', '1x', 0);
+    emit Buy(msg.sender, msg.value, tokenNumber, tokenUnitValue);
   }
+  
+  
+  function reedeem(uint32 tokenId, uint32 tokenUnits) public  {
+      
+    // validate sender owns token
+    require(msg.sender == ownerOf(tokenId));
+    
+    // transfer tokenUnitValue to contract owner
+    
+    
+    // emit event
+    emit Transfer(msg.sender, tokenUnits);    
+  }
+  
+  function sell(uint32 tokenId) payable public  {
+      
+    // validate:
+    // sender owns token
+    
+    // burn token
+    
+    // return value form contract
 
-  /*
-  * getters
-  */
-  // function getContractBalance() public view returns(uint256 balance) {
-  //     balance = this.balance;
-  // }
+  }
+  
+  
 
-  // function isFunder(address addr) public view returns(bool) {
-  //     return funders[addr].exists;
-  // }
-
-  // function getFunderTokens(address addr) public view returns(uint256) {
-  //     return funders[addr].tokenCount;
-  // }
-
-  // function getFunderPurchase(address addr) public view returns(uint256) {
-  //     return funders[addr].totalPurchasePrice;
-  // }
-
-  // function getCurrentTotalFunders() public view returns(uint) {
-  //     return totalCurrentFunders;
-  // }
+  function _calcPurchaseReturn(uint256 valueSent) public view returns (uint256) {
+    return valueSent.div(tokenBasePrice);
+  }  
+  
+  function _getTokenValue(uint256 _tokenId) public view returns (uint256) {
+    return tokenValues[_tokenId];
+  }
+  function _setTokenValue(uint256 _tokenId, uint256 _value) internal {
+    tokenValues[_tokenId] = _value;
+  }
+  
 
 }
